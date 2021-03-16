3a051c48b02faa6ae51a7cedd96f720a1fbf307c
Vincent Yiu <vysec.private@gmail.com>
2021-03-16 23:29:10 +0800

    Fix log path

--- LinuxForwarder/install-forwarder.sh
+++ LinuxForwarder/install-forwarder.sh
@@ -4,7 +4,7 @@ dpkg -i splunkforwarder*.deb
 /opt/splunkforwarder/bin/splunk add forward-server 127.0.0.1:9997
 /opt/splunkforwarder/bin/splunk start
 /opt/splunkforwarder/bin/splunk add monitor /var/log/auth.log -index ssh -sourcetype %APP%
-/opt/splunkforwarder/bin/splunk add monitor /root/cobaltstrike/.../weblog.log       -index cobalt -sourcetype weblog      
+/opt/splunkforwarder/bin/splunk add monitor /root/cobaltstrike/.../weblog_*.log       -index cobalt -sourcetype weblog      
 /opt/splunkforwarder/bin/splunk add monitor /root/cobaltstrike/.../beacon_*.log     -index cobalt -sourcetype beacon_log  
 /opt/splunkforwarder/bin/splunk add monitor /root/cobaltstrike/.../keystrokes_*     -index cobalt -sourcetype keystrokes       
 /opt/splunkforwarder/bin/splunk add monitor /root/cobaltstrike/.../events.log       -index cobalt -sourcetype teamserver_events
